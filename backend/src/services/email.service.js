3ae4826d93dda85676949a8099e4008b2f2d7809
Arpitkatiyyar <arpitkatiyarhome@gmail.com>
2026-06-22 21:38:36 +0530

    deployment

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,35 +1,62 @@
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-dotenv.config();
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+// import nodemailer from "nodemailer";
+// import dotenv from "dotenv";
+// dotenv.config();
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// export const sendOTPEmail = async (email, otp) => {
+//   try{await transporter.sendMail({
+//     from: process.env.EMAIL_USER,
+
+//     to: email,
+
+//     subject: "IntelliStudy Verification OTP",
+
+//     html: `
+//       <div>
+//         <h2>Email Verification</h2>
+//         <p>Your OTP is:</p>
+//         <h1>${otp}</h1>
+//         <p>Valid for 5 minutes.</p>
+//       </div>
+//     `,
+//   });}
+//   catch(error){
+//     console.error("Error sending email:",error)
+//     throw error;
+//   }
+// };
+
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendOTPEmail = async (email, otp) => {
-  try{await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "IntelliStudy Verification OTP",
+      html: `
+        <div>
+          <h2>Email Verification</h2>
+          <p>Your OTP is:</p>
+          <h1>${otp}</h1>
+          <p>Valid for 5 minutes.</p>
+        </div>
+      `,
+    });
 
-    to: email,
-
-    subject: "IntelliStudy Verification OTP",
-
-    html: `
-      <div>
-        <h2>Email Verification</h2>
-        <p>Your OTP is:</p>
-        <h1>${otp}</h1>
-        <p>Valid for 5 minutes.</p>
-      </div>
-    `,
-  });}
-  catch(error){
-    console.error("Error sending email:",error)
+    console.log("OTP email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
     throw error;
   }
 };
